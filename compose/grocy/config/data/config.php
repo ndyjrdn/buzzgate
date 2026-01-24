bce9a722829c8cd6c2a342be02e8d871dd66344d
ndyjrdn <51726610+ndyjrdn@users.noreply.github.com>
2026-01-24 13:10:23 -0600

    Update config.php

--- compose/grocy/config/data/config.php
+++ compose/grocy/config/data/config.php
@@ -2,9 +2,8 @@
 return [
     'AUTHENTICATION' => 'HTTP_BASIC',
     'AUTHENTICATION_API' => 'API_KEY',
-
-    // Set your credentials here
     'HTTP_BASIC_AUTH_USER' => 'andy',
     REDACTED_GROCY_PW,
 ];
+
 
